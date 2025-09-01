008c910284326947d488c9e11fb8eb755a9e9dd5
ScaredBow <31490393+ScaredBow@users.noreply.github.com>
2025-08-31 20:38:55 -0400

    Initial start of profile command

--- main.py
+++ main.py
@@ -23,6 +23,10 @@ async def on_ready():
 async def hello(ctx: discord.ApplicationContext):
     await ctx.respond("Hey!")
 
+@bot.slash_command(name="help", description="")
+async def help(ctx: discord.ApplicationContext):
+    await ctx.respond("Hi! For assistance, please contact @ccc")
+
 from notion import add_entry_to_person_property
 @bot.slash_command(name="register", description="Register your Discord account")
 async def register(ctx: discord.ApplicationContext, first_name: str):
@@ -41,6 +45,22 @@ async def register(ctx: discord.ApplicationContext, first_name: str):
         print(f"Failed {first_name} with ID {user_id}")
         await msg.edit_original_response(content=f"⚠️ Could not find {first_name} in the database. Please try again.")
 
+@bot.slash_command(name="more", description="Advanced Settings")
+async def more(ctx: discord.ApplicationContext):
+    await ctx.respond("work in progress")
 
+from discord import Option
+@bot.slash_command(name="profile", description="Retrieve your profile information")
+async def profile(
+    ctx: discord.ApplicationContext,
+    first_name: Option(str, "First name of the person to look up. Returns yourself if empty.") = None,
+    search_depth: Option(int, "Level of search (0, 1, 2, 3). Restricted parameter.") = None
+    ):
+    msg = f'Profile for {ctx.author}'
+    if first_name:
+        msg += f"\nLooking up first name: {first_name}"
+    if search_depth:
+        msg += f"\nSearch depth: {search_depth}"
+    await ctx.respond(msg) 
 
 bot.run(TOKEN) # run the bot with the token
